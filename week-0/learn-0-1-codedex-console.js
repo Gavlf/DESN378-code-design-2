c6da79d808e78efd4f1452bccbe8f3979849909a
Gavin Figueroa <gfigueroa@ewu.edu>
2026-01-11 23:43:22 +0000

    Add learn 0-1 Codedex exercises

---
+++ week-0/learn-0-1-codedex-console.js
@@ -0,0 +1,83 @@
+console.log("============================================");
+console.log("TUTORIAL 0-1: CODEDEX CONSOLE FUNDAMENTALS");
+console.log("Student: Gavin Figueroa");
+console.log("Date: 1/11/2026");
+console.log("============================================");
+
+console.log("--------------------------------------------");
+console.log("EXERCISE 1: SETTING UP");
+console.log("--------------------------------------------");
+// Add your custom greeting below:
+console.log("Hello internet!");
+console.log("--------------------------------------------");
+console.log("EXERCISE 2: THE CONSOLE");
+console.log("Your favorite food");
+console.log("--------------------------------------------");
+console.log("🍌 Banana!");
+
+console.log("--------------------------------------------");
+console.log("EXERCISE 3: LETTER TREE");
+console.log("The pattern: a, b c, d e f, g h i j, k");
+console.log("--------------------------------------------");
+console.log("   a   ");
+console.log("  b c  ");
+console.log(" d e f" );
+console.log("g h i j");
+console.log("   k   ");
+
+console.log("--------------------------------------------");
+console.log("EXERCISE 4: SECRET RECIPE");
+console.log("Use comments to document your recipe");
+console.log("Include a \"secret ingredient\" comment");
+console.log("--------------------------------------------");
+console.log("Perfect Homeade French Bread");
+console.log("  ");
+console.log("Learn how to make the perfect homeade french bread.");
+console.log("It only requires a handful of ingredients to make perfect bakery style bread at home!");
+console.log("  ");
+console.log(" Ingredients:");
+console.log("  ");
+console.log("  . 1 1/2 tablespoons instant dry yeast");
+console.log("  . 2 cups warm water");
+console.log("  . 1 1/2 teaspoons salt");
+console.log("  . 4 1/2 to 5 cups all-purpose flour");
+//*egg can be substituted with olive oil or butter//
+console.log("  . 1 large egg beaten");
+console.log("  ");
+console.log(" Instructions:");
+console.log("  ");
+/* This recipe requires a couple of hours to finish!
+It's best made on a weekend or a vacation when you are binging a show or watching a movie.
+*/
+console.log("  1. In the bowl of a stand mixer, add 1 1/2 tablespoons instant dry yeast, 2 cups warm water, 1 1/2 teaspoons salt, and about 4 to 4 1/2 cups of flour. Knead on a slow speed until the flour has started to combine. Increase speed to medium and gradually add in remaining flour, just until dough pulls away from the sides of the bowl. The exact amount of flour you need varies based on a variety of factors. You should have a soft pliable dough that is tacky, but not sticky to the touch.");
+console.log("  ");
+console.log("  2. Transfer to a lightly greased mixing bowl, cover with a towel, and let rise until double in size, about 1 hour.");
+console.log("  ");
+console.log("  3. Divide dough in half. Roll each half into a large rectangle. Roll up tightly from the long end. Place onto a lightly greased baking sheet or into a French bread pan. Cover and let rise 30 to 45 minutes.");
+console.log("  ");
+console.log("  4. Meanwhile, preheat oven to 375 degrees and use a fork or whisk to beat 1 large egg in a small bowl.");
+console.log("  ");
+console.log("  5. Use a sharp knife to cut diagonal slashes on the top of each bread loaf, about 1/4 inch deep. Brush loaves with beaten egg.");
+console.log("  ");
+console.log("  6. Bake in preheated oven for 35-40 minutes, until golden brown. Remove from the oven and cool on a wire rack before serving.");
+
+console.log("--------------------------------------------");
+console.log("EXERCISE 5: RECEIPT");
+console.log("Format a receipt from a recent purchase");
+console.log("--------------------------------------------");
+console.log("=================================")
+console.log("       Amazon.com RECEIPT       ")
+console.log("---------------------------------")
+console.log("         Order Details        ")
+console.log("---------------------------------")
+console.log("Item                      Price")
+console.log("---------------------------------")
+console.log("Wicked: For Good          29.99")
+console.log("         ")
+console.log("=================================")
+console.log("Item Subtotal:            29.99")
+console.log("Tax Collected:             2.73")
+console.log("---------------------------------")
+console.log("Total:                    32.72")
+console.log("=================================")
+console.log("============================================");
