209f62fdec1d86db36940ba2a7e088614ed762e0
Gavin Figueroa <gfigueroa@ewu.edu>
2026-01-12 03:04:09 +0000

    Refactor code structure for improved readability and maintainability

--- week-0/behavioral-layer/scripts/main.js
+++ week-0/behavioral-layer/scripts/main.js
@@ -1,16 +1,32 @@
 // Store a reference to the <h1> in a variable
 const myHeading = document.querySelector("h1");
 // Update the text content of the <h1>
-myHeading.textContent = "J  E  N  N  I  E";
+myHeading.textContent = "J   E   N   N   I   E";
 // Store a reference to the <img> in a variable
 const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/placeholder-1.jpg") {
-    myImage.setAttribute("src", "images/placeholder-2.jpg");
+  if (mySrc === "images/jennie-1.jpg") {
+    myImage.setAttribute("src", "images/jennie-2.jpg");
   } else {
-    myImage.setAttribute("src", "images/placeholder-1.jpg");
+    myImage.setAttribute("src", "images/jennie-1.jpg");
   }
 });
 
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `J   E   N   N   I   E, ${myName}`;
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `J   E   N   N   I   E, ${storedName}`;
+}
+myButton.addEventListener("click", () => {
+  setUserName();
+});
