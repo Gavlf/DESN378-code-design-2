b234510a0b6d74330ab877f3a960635280da2185
Gavin Figueroa <gfigueroa@ewu.edu>
2026-01-19 05:09:39 +0000

    final edits to story-teller project

--- week-1/story-teller/scripts/main.js
+++ week-1/story-teller/scripts/main.js
@@ -1,15 +1,39 @@
+// Find elements
+const image = document.querySelector('.story-image img');
+const header = document.querySelector('.story-header');
+const title= document.querySelector('#story-title');
+
+
+// Define variables
+const totalImages = 5;  
+
 // Track current step
 let currentStep = 0;
+
+// Change title color on image hover
+image.addEventListener('mouseenter', function() {
+  title.style.color = '#c6341d';
+});
+
+image.addEventListener('mouseleave', function() {
+  title.style.color = 'white';
+});
 
 // Interactive clicks on image to advance story
 image.addEventListener('click', function() {
   currentStep++;
-  if (currentStep < captions.length) {
-    // Update caption
-    caption.textContent = captions[currentStep];
-    // Update image
-    image.src = `images/man-run${currentStep + 1}.jpg`;
-    image.setAttribute('data-step', currentStep + 1);
+  if (currentStep < totalImages) {
+      // Fade out
+    image.style.opacity = '0';
+    
+    setTimeout(function() {
+      image.onload = function() {
+        header.style.display = 'none';
+        image.style.opacity = '1';
+      };
+      
+      // Update image
+      image.src = `images/image${currentStep + 1}.jpg`;
+    }, 500);
   }
 });
-​
